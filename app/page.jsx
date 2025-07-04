9a0e8640aacde3e6545e7cfb8a2d56cd562fa87f
Akshay <90892081+aksh-mon@users.noreply.github.com>
2025-07-04 16:52:52 +0530

    hellonew

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Article from 'components/article'
+import Article from '../components/article'
 const page = () => {
   return (
     <div>
       Welcome to Aksh Daily Blogs ....
       <Article/>
+      hello man hwz u
       </div>
   )
 }
